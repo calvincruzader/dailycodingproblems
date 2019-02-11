ab9b4d02a3acd406931edb149c951496bea1691c
Calvin <calvincruzader@gmail.com>
2019-02-10 19:13:40 -0600

    solves dcp2

--- dailycodingproblem2.py
+++ dailycodingproblem2.py
@@ -1,19 +1,28 @@
-# class Solution: 
-# 	def product_except_at_i(self, arr): 
-# 		leftSide = [1 for _ in range(len(arr))]
-# 		rightSide = [1 for _ in range(len(arr))] 
+class Solution: 
+  def product_except_at_i(self, arr): 
+    leftSide = [1 for _ in range(len(arr))]
+    rightSide = [1 for _ in range(len(arr))] 
 
-# 		productLeft = 1
-# 		for i in range(len(arr)):
+    productLeft = 1
+    for i in range(len(arr)):
+      leftSide[i] = productLeft
+      productLeft  *= arr[i]
 
+    productRight = 1
+    for i in range(len(x)-1, -1, -1):
+      rightSide[i] = productRight 
+      productRight *= arr[i]
 
+    output = [] 
+    for i in range(len(arr)): 
+      output.append(leftSide[i] * rightSide[i])
 
-# 		productRight = 1
-# 		for i in range(len(arr)-1, 0, -1):
+    return output
 
-# s = Solution()
+s = Solution()
 
-
-x = [1,2,3,4]
-for i in range(len(x)-1, -1, -1):
-	print("{0} {1}".format(i, x[i]))
+x = [1,2,3,4, 5]
+# for i in range(len(x)-1, -1, -1):
+# 	print("{0} {1}".format(i, x[i]))
+#   
+print(s.product_except_at_i(x))
