30466f574563cd6eca323257c56154dea634289c
Calvin <calvincruzader@gmail.com>
2019-02-23 21:49:58 -0600

    begins solution for dcp4

--- dailycodingproblem4.py
+++ dailycodingproblem4.py
@@ -1,3 +1,20 @@
 '''
-Given an unsorted incremental array, find the number that is missing.
+Given an array of integers, find the first missing positive integer in linear time and constant space. 
+In other words, find the lowest positive integer that does not exist in the array. 
+The array can contain duplicates and negative numbers as well.
+
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+
+You can modify the input array in-place.
 '''
+class Solution: 
+
+  def find_first_missing_int(self, arr):
+
+
+
+
+x = [1,2,3,4,6,7]
+s = Solution() 
+print(s.find_first_missing_int(x))
+
