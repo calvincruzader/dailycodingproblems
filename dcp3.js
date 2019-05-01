8d05017dcc42a7f3084a54a95f4503fa69dfc95b
Calvin Cruzada <ccruzada@redventures.com>
2019-05-01 18:34:30 -0500

    finishes dcp3

--- dcp3.js
+++ dcp3.js
@@ -27,33 +27,57 @@ function mySerialize(root) {
   let my_q = [];
   my_q.push(root);
   let serialized = '';
-  while(my_q) {
-
+  while(my_q.length > 0) {
     let val = my_q.shift();
-  
-    serialized += val;
-    if(val === '#') continue; 
+
+    if(val === '#') {
+      serialized += val + " ";
+      continue;
+    } else { 
+      serialized += val.val + " ";
+    }
     
-    if(my_q.left) { 
-      my_q.push(my_q.left);
+    if(val.left) { 
+      my_q.push(val.left);
     } else { 
       my_q.push('#');
     }
 
-    if(my_q.right) { 
-      my_q.push(my_q.right);
+    if(val.right) { 
+      my_q.push(val.right);
     } else { 
       my_q.push('#');
     }
-    break;
   }
   return serialized;
 }
 
+function myDeserialize(serial) { 
+  let serial_arr = serial.split(' ');
+  
+  for(let i = 0; i < serial_arr.length; i++) { 
+    serial_arr[i] = new Node(serial_arr[i]);
+  }
+
+  for(let i = 0; i < serial_arr.length; i++) { 
+    if(serial_arr[i] === '#') { continue; }
+    let leftChildIdx = 2 * i + 1;
+    let rightChildIdx = 2 * i + 2;
+    if(leftChildIdx < serial_arr.length && serial_arr[leftChildIdx] !== '#') { 
+      serial_arr[i].left = serial_arr[leftChildIdx];
+    }
+    if(rightChildIdx < serial_arr.length && serial_arr[rightChildIdx] !== '#') { 
+      serial_arr[i].right = serial_arr[rightChildIdx];
+    }
+
+  }
+  return serial_arr[0];
+}
+
 let root = new Node('root', new Node('left', new Node('left.left')), new Node('right'));
-console.log(mySerialize(root)); 
 
+let s = mySerialize(root);
+let d = myDeserialize(s);
 
-
-
-
+console.log(s);
+console.log(d.left.left.val);
